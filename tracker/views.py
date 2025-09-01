6b1d677c50f150c6d36a88346c59d2f9868e96f7
M-MuneebAlam <mmuneeb.alam09@gmail.com>
2025-09-01 23:25:59 +0500

    Display Transactions | django-filter FilterSet

--- tracker/views.py
+++ tracker/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Transaction
+from .filters import TransactionFilter
 
 # Create your views here.
 def index(request):
     return render(request, 'tracker/index.html')
+
+
+# @login_required
+# def transactions_list(request):
+#     transaction_filter = TransactionFilter(
+#         request.GET,
+#         queryset=Transaction.objects.filter(user=request.user).select_related('category')
+#     )
+#     context = {'filter': transaction_filter}
+
+#     if request.htmx:
+#         return render(request, 'tracker/partials/transactions-container.html', context)
+
+#     return render(request, 'tracker/transactions-list.html', context)
+
+@login_required
+def transactions_list(request):
+    transaction_filter = TransactionFilter(
+        request.GET,
+        queryset=Transaction.objects.filter(user=request.user)
+    )
+    context = {'filter': transaction_filter}
+    return render(request, 'tracker/transactions-list.html', context)
